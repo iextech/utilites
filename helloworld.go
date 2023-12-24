de251111d7c5a26be61cf58e3d24b78dfde8be8e
DaeGon Kim <daegonk@gmail.com>
2023-12-24 22:29:18 +0000

    testing

--- helloworld.go
+++ helloworld.go
@@ -2,8 +2,11 @@ package main
 
 import (
     "fmt"
+    //"github.com/tomofumi-corespeq/hello v0.1.0"
+    "github.com/tomofumi-corespeq/hello"
 )
 
 func main() {
     fmt.Println("My favorite number is")
+    hello.World()
 }
